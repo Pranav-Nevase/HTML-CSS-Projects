2d364de156e1da271a632298427d302044f3f37f
Pranav-Nevase <pranavnevase010@gmail.com>
2026-09-10 08:58:35 +0530

    Result sumary component

---
+++ Results summary component/index.js
@@ -0,0 +1,10 @@
+// 1. Grab the button from your HTML
+const continueBtn = document.querySelector('.button button');
+
+// 2. Tell the button to listen for a 'click'
+continueBtn.addEventListener('click', function() {
+    
+    // 3. Put your action inside the function!
+    alert("Results summary component");
+    
+});
